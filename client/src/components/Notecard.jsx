e516f48e8fba19fdda97acb02fe8700fac392985
Hari-git04 <harigattem04@gmail.com>
2025-10-18 19:30:37 +0530

    responsive

--- client/src/components/Notecard.jsx
+++ client/src/components/Notecard.jsx
@@ -1,45 +1,55 @@
 import { PenSquareIcon, Trash2Icon } from 'lucide-react';
 import React from 'react'
 import { formatDate } from '../../../server/lib/utils';
-import { Link } from 'react-router';
+import { Link } from 'react-router'; 
 import axios from 'axios'
 import toast from 'react-hot-toast'
 
 function Notecard ({note,setNotes}) {
 
+  const handleDelete=async(e,id)=>{
+    e.preventDefault();
 
-const handleDelete=async(e,id)=>{
-  e.preventDefault();
+    if (!window.confirm("Are You Sure You Want To Delete?")) return;
 
-if (!window.confirm("Are You Want To Delete")) return;
-
-try {
+    try {
       await axios.delete(`http://localhost:5000/note/${id}`)
       setNotes((pre)=>pre.filter(note=>note._id !==id))
-      toast.success("Note delete successfull")
-}
- catch (error) {
-  console.log(error);
-  toast.error("Failed to delete note")
-  
-}
-};
+      toast.success("Note deleted successfully")
+    }
+    catch (error) {
+      console.log(error);
+      toast.error("Failed to delete note")
+    }
+  };
 
   return (
-    <div className="m-8 font-mono">
-      <div className="card bg-primary text-primary-content w-96">
+    <div className="m-4 font-mono flex justify-center">
+      <div className="card bg-primary text-primary-content w-full max-w-sm sm:max-w-md md:max-w-lg">
         <Link to={`/note/${note._id}`}>
-        <div className="card-body">
-          <h2 className="card-title">{note.title}</h2>
-          <p>{note.content}</p>
-          <span className='text-sm  text-white text-base-content/60'>
-          {formatDate(new Date(note.createdAt))}
-          </span>
-          <div className="card-actions justify-end gap-2">
-            <button className="btn btn-ghost min-h-0 h-8 px-2"><PenSquareIcon className='size-5'/></button>
-            <button className="btn btn-ghost min-h-0 h-8 px-2" onClick={(e)=>handleDelete(e,note._id)}><Trash2Icon className='size-5 text-[#ff0000]'/></button>
+          <div className="card-body flex flex-col gap-2">
+
+            <h2 className="card-title text-lg sm:text-xl truncate">{note.title}</h2>
+
+            <p className="text-sm sm:text-base line-clamp-3 break-words">{note.content}</p>
+
+            <span className='text-xs sm:text-sm text-white text-base-content/60'>
+              {formatDate(new Date(note.createdAt))}
+            </span>
+
+            <div className="card-actions flex flex-col sm:flex-row justify-end gap-2 mt-2">
+              <button className="btn btn-ghost min-h-0 h-8 px-2">
+                <PenSquareIcon className='size-5'/>
+              </button>
+              <button 
+                className="btn btn-ghost min-h-0 h-8 px-2" 
+                onClick={(e)=>handleDelete(e,note._id)}
+              >
+                <Trash2Icon className='size-5 text-[#ff0000]'/>
+              </button>
+            </div>
+
           </div>
-        </div>
         </Link>
       </div>
     </div>
